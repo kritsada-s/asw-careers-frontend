bb70feeb342cd41b0d27cda86125eecc28965c15
kritsadas <kritsada.s@assetwise.co.th>
2024-12-02 13:36:35 +0700

    separate client and server component

--- app/apply-job/page.tsx
+++ app/apply-job/page.tsx
@@ -1,25 +1,7 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { useParams } from 'next/navigation';
-import LoaderHorizontal from '../components/ui/loader';
-
 function ApplyJobPage() {
-  const params = useParams();
-  const jobId = params.id as string;
-
-  // useEffect(() => {
-  //   if (!jobId) {
-  //     if (typeof window !== 'undefined') {
-  //       sessionStorage.getItem('jobId')
-  //     }
-  //   }
-  // }, [jobId]);
-
   return (
     <div className='flex justify-center items-center h-screen'>
-      <p>Apply Job Page for {sessionStorage.getItem('jobId')}</p>
+      <p>Apply Job Page</p>
     </div>
   );
 }
