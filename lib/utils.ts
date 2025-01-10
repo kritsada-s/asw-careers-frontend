c15aad66550c409dd4302371ba257255f3984aa9
kritsadas <kritsada.s@assetwise.co.th>
2025-01-10 15:41:40 +0700

    restore server var

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,11 +12,9 @@ export const defaultID = "00000000-0000-0000-0000-000000000000";
 //export const devUrl = "https://dev-career.assetwise.co.th/wrsapi";
 export const bConnectionID = "7B93F134-D373-4227-B5A6-6B619FF0E355";
 
-// export const prodUrl = typeof window !== 'undefined' && (window.location.hostname === 'wrs-careers.web.app' || window.location.hostname === 'localhost') 
-//   ? "https://aswservice.com/wrsapiuat" 
-//   : "https://aswservice.com/wrsapi";
-
-export const prodUrl = "https://aswservice.com/wrsapiuat";
+export const prodUrl = typeof window !== 'undefined' && (window.location.hostname === 'wrs-careers.web.app' || window.location.hostname === 'localhost') 
+  ? "https://aswservice.com/wrsapiuat" 
+  : "https://aswservice.com/wrsapi";
 
 export async function getCompanyByID (companyID:string) {
   try {
